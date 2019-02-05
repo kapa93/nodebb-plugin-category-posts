27df10dd06bb92ba683c59cb2343a2ada371fe39
Kacper Myslinski <kacpermyslinski@gmail.com>
2019-02-05 16:32:14 -0600

    adds widget

--- library.js
+++ library.js
@@ -22,10 +22,48 @@ plugin.addAdminNavigation = function (header, callback) {
 	header.plugins.push({
 		route: '/plugins/category-posts',
 		icon: 'fa-tint',
-		name: 'Category-posts',
+		name: 'Category Posts',
 	});
 
 	callback(null, header);
 };
+
+plugin.defineWidgets = function(widgets, callback) {
+	var widget = {
+		widget: "categoryPosts",
+		name: "Category Posts",
+		description: "List of categories with their posts.",
+	};
+	app.render('admin/plugins/nodebb-plugin-category-posts/widget', {}, function (err, html) {
+		if (err) {
+			return callback(err);
+		}
+		widget.content = html;
+		widgets.push(widget);
+		callback(null, widgets);
+	});
+};
+
+plugin.renderWidget = function(widget, callback) {
+	var data = {
+		templateData: {
+			config: {
+				relative_path: nconf.get('relative_path'),
+			},
+		},
+		req: {
+			uid: widget.uid,
+		},
+		cid: widget.data.cid || 0,
+	};
+
+	app.render('partials/nodebb-plugin-category-posts/header', data.templateData, function (err, html) {
+		if (err) {
+			return callback(err);
+		}
+		widget.html = html;
+		callback(null, widget);
+	});
+}
 
 module.exports = plugin;
