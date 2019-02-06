6c7759cc13b8551ed231572646313a3614778013
Kacper Myslinski <kacpermyslinski@gmail.com>
2019-02-06 10:02:16 -0600

    added admin view

--- library.js
+++ library.js
@@ -20,8 +20,6 @@ plugin.init = function (params, callback) {
 	// We create two routes for every view. One API call, and the actual route itself.
 	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
 
-	router.get('/admin/plugins/category-posts', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
-	router.get('/api/admin/plugins/category-posts', controllers.renderAdminPage);
 	router.get('/plugins/nodebb-plugin-category-posts/render', renderExternal);
 
 	callback();
@@ -54,18 +52,6 @@ plugin.defineWidgets = function(widgets, callback) {
 };
 
 plugin.renderWidget = function(widget, callback) {
-	var data = {
-		templateData: {
-			config: {
-				relative_path: nconf.get('relative_path'),
-			},
-		},
-		req: {
-			uid: widget.uid,
-		},
-		cid: widget.data.cid || 0,
-	};
-
 	router.render('partials/nodebb-plugin-category-posts/header', function (err, html) {
 		if (err) {
 			return callback(err);
---
+++ static/templates/admin/plugins/nodebb-plugin-category-posts/widget.tpl
@@ -0,0 +1,1 @@
+<span>Yay</span>
